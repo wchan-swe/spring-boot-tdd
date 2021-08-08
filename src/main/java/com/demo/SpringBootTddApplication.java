45c85928d190caa20f5318020a22c3765f4da8d8
Wayne Chan <wchan.it@outlook.com>
2021-08-08 05:09:31 -0500

    initial commit

---
+++ src/main/java/com/demo/SpringBootTddApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTddApplication.class, args);
+    }
+
+}
